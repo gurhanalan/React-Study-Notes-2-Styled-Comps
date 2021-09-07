9cccc504733b1cba5b6569d771af3021dd34983e
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-09-07 14:12:45 +0300

    Add second button

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     return (
         <div className="App">
             <Button>Click Me</Button>
+            <Button primary>Primary</Button>
         </div>
     );
 }
--- src/components/styled-comp/button/Button.styles.jsx
+++ src/components/styled-comp/button/Button.styles.jsx
@@ -2,16 +2,17 @@ import styled from "styled-components";
 
 export const Button = styled.button`
     border: none;
-    margin: 2px;
-    color: blue;
-    height: 2rem;
-    width: 4rem;
-    background-color: chocolate;
+    margin: 1rem;
+    color: white;
+    padding: 1rem 2rem;
+    text-align: center;
+    background-color: ${(props) => (props.primary ? "blue" : "purple")};
     border-radius: 0.5rem;
     box-shadow: 0 1rem 4rem black;
+    font-size: 1.5rem;
     transition: all 0.3s;
     &:hover {
         background-color: green;
-        transform: scale(1.2);
+        transform: scale(1.1);
     }
 `;
